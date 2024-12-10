9b3486ce4db33dc576a66430bf0cf15a6eb0098d
Mohamed <mamankour@outlook.be>
2024-12-10 01:07:28 +0100

    Profile details

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from .views import UserSignUpView
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', UserSignUpView.as_view(), name='user-signup'),
+    path("signup/", UserSignUpView.as_view(), name="user-signup"),
+    path('profile/', profile, name='user-profile'),
 ]
-
