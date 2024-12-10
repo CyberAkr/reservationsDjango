e1c24c277655e852d357f8be131f2460c8c46456
Mohamed <mamankour@outlook.be>
2024-12-10 01:04:10 +0100

    Frontend authentification - Advenced signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 class UserSignUpView(UserPassesTestMixin, CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
