304dac35e47fbe70447eb3781e51021954ad9b6e
Mohamed <mamankour@outlook.be>
2024-12-10 01:13:16 +0100

    Profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import UserSignUpView
+from .views import UserSignUpView, UserUpdateView
+from .views import profile
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path("signup/", UserSignUpView.as_view(), name="user-signup"),
+    path('signup/', UserSignUpView.as_view(), name='user-signup'),
     path('profile/', profile, name='user-profile'),
+    path('profile/<int:pk>/', UserUpdateView.as_view(), name='user-update'),
 ]
+
